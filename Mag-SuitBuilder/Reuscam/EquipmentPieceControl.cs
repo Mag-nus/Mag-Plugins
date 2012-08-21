7926ca447eb52e8ff48e289ff1eb7490a9098c91
reuscam_cp <SND\reuscam_cp>
2012-08-21 00:31:01 +0000

    Added tooltip text on the lock chkBox, to determine what each EquipmentControl was slotted for

--- Mag-SuitBuilder/Reuscam/EquipmentPieceControl.cs
+++ Mag-SuitBuilder/Reuscam/EquipmentPieceControl.cs
@@ -10,13 +10,38 @@ namespace Mag_SuitBuilder
 		public EquipmentPieceControl()
 		{
 			InitializeComponent();
+           
 		}
 
 		public bool IsLocked { get { return chkLocked.Checked; } }
 
 		string IEquipmentPiece.Name { get { return lblItemName.Text; } }
 
-		public Constants.EquippableSlotFlags EquipableSlots { get; set; }
+        private Constants.EquippableSlotFlags mEquipableSlots;
+		public Constants.EquippableSlotFlags EquipableSlots {
+            get
+            {
+                return mEquipableSlots;
+            }
+
+            set
+            {
+                mEquipableSlots = value;
+                // Create the ToolTip and associate with the Form container.
+                ToolTip toolTip1 = new ToolTip();
+
+
+                // Set up the delays for the ToolTip.
+                toolTip1.AutoPopDelay = 5000;
+                toolTip1.InitialDelay = 1000;
+                toolTip1.ReshowDelay = 500;
+                // Force the ToolTip text to be displayed whether or not the form is active.
+                toolTip1.ShowAlways = true;
+
+                // Set up the ToolTip text for the Button and Checkbox.
+                toolTip1.SetToolTip(this.chkLocked, this.EquipableSlots.ToString());
+            }
+        }
 
 		public int NumberOfSlotsCovered { get { return 1; } }
 
