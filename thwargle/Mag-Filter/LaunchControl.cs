6c9d59f92e12a206962a10dada8d21519be88829
thwargle_cp <SND\thwargle_cp>
2015-11-25 21:42:35 +0000

    Added better error handling for datetimes. If the date is invalid, we don't hard crash.

--- thwargle/Mag-Filter/LaunchControl.cs
+++ thwargle/Mag-Filter/LaunchControl.cs
@@ -30,28 +30,38 @@ namespace MagFilter
                     string[] lines = contents.Split(stringSeps, StringSplitOptions.RemoveEmptyEntries);
                     if (lines.Length == 4)
                     {
-                        if (lines[0].StartsWith("ServerName:")
-                            && lines[1].StartsWith("AccountName:")
-                            && lines[2].StartsWith("CharacterName:")
-                            && lines[3].StartsWith("Time:")
+                        if (BeginsWith(lines[0], "ServerName:")
+                            && BeginsWith(lines[1], "AccountName:")
+                            && BeginsWith(lines[2], "CharacterName:")
+                            && BeginsWith(lines[3], "Time:")
                             )
                         {
                             info.ServerName = lines[0].Substring("ServerName:".Length);
                             info.AccountName = lines[1].Substring("AccountName:".Length);
                             info.CharacterName = lines[2].Substring("CharacterName:".Length);
-                            info.LaunchTime = DateTime.Parse(lines[3].Substring("Time:".Length));
-                            TimeSpan maxLatency = new TimeSpan(0, 0, 5, 0);
-                            if (DateTime.UtcNow - info.LaunchTime < maxLatency)
+                            DateTime parsedLaunchTime;
+                            if (DateTime.TryParse(lines[3].Substring("Time:".Length), out parsedLaunchTime))
                             {
-                                info.IsValid = true;
+                                info.LaunchTime = parsedLaunchTime;
+                                TimeSpan maxLatency = new TimeSpan(0, 0, 5, 0);
+                                if (DateTime.UtcNow - info.LaunchTime < maxLatency)
+                                {
+                                    info.IsValid = true;
+                                }
                             }
                         }
                     }
                 }
             }
-
-            
             return info;
+        }
+        /// <summary>
+        /// Line starts with specified prefix and has at least one character beyond it
+        ///  (primarily used to Substring(prefix.Length) will not fail
+        /// </summary>
+        private bool BeginsWith(string line, string prefix)
+        {
+            return line != null && line.StartsWith(prefix) && line.Length > prefix.Length;
         }
         public void RecordLaunchInfo(string serverName, string accountName, string characterName, DateTime timestamp)
         {
