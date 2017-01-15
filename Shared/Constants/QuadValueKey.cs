cd6fc5663dde8de8edaef9d57a2137d241de9b63
Mag_nus_cp <Mag-nus@users.noreply.github.com>
2017-01-15 01:21:33 +0000

    QuadValueKey added

---
+++ Shared/Constants/QuadValueKey.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Mag.Shared.Constants
+{
+	public enum QuadValueKey
+	{
+		TotalExperience = 0x01,
+		UnassignedExperience = 0x02,
+		ItemCurrentExperience = 0x04,
+		ItemLevelExperienceIncrement = 0x05,
+		LuminancePointsCurrent = 0x06,
+		LuminancePointsMaximum = 0x07,
+	}
+}
