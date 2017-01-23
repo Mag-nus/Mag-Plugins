6944849cabda7d690294c7404da8a593161a0a13
Mag_nus_cp <Mag-nus@users.noreply.github.com>
2017-01-23 16:42:51 +0000

    Mag-CombatLogger almost done

--- Mag-CombatLogger/Class1.cs
+++ Mag-CombatLogger/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -28,6 +29,8 @@ namespace Mag_CombatLogger
 
 			Core.EchoFilter.ServerDispatch += EchoFilter_ServerDispatch;
 			Core.EchoFilter.ClientDispatch += EchoFilter_ClientDispatch;
+
+			Core.ItemSelected += Core_ItemSelected;
 			Core.CharacterFilter.SpellCast += CharacterFilter_SpellCast;
 
 			Core.ChatBoxMessage += Core_ChatBoxMessage;
@@ -39,16 +42,9 @@ namespace Mag_CombatLogger
 
 			Core.EchoFilter.ServerDispatch -= EchoFilter_ServerDispatch;
 			Core.EchoFilter.ClientDispatch -= EchoFilter_ClientDispatch;
+
+			Core.ItemSelected += Core_ItemSelected;
 			Core.CharacterFilter.SpellCast -= CharacterFilter_SpellCast;
-		}
-
-
-		private void EchoFilter_ClientDispatch(object sender, NetworkMessageEventArgs e)
-		{
-			try
-			{
-			}
-			catch { }
 		}
 
 
@@ -67,6 +63,173 @@ namespace Mag_CombatLogger
 			return true;
 		}
 
+
+		private void EchoFilter_ClientDispatch(object sender, NetworkMessageEventArgs e)
+		{
+			try
+			{
+				if (e.Message.Type == 0xF7B1) // Game Event
+				{
+					var actionId = e.Message.Value<uint>("action");
+
+					if (actionId == 7) // Evt_Combat__UntargetedMeleeAttack_ID
+					{
+						if (!CheckForAetheriaAndCloak())
+							return;
+
+						var logFileName = CreateFileIfItDoesntExist();
+
+						AddCurrentVariablesToLog(logFileName);
+
+						using (StreamWriter writer = new StreamWriter(logFileName, true))
+						{
+							StringBuilder output = new StringBuilder();
+
+							// "Timestamp,Landcell,JSON"
+							output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
+							output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
+
+							using (MemoryStream memoryStream = new MemoryStream(e.Message.RawData))
+							using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+							{
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+
+								output.Append("\"{\"ClientDispatch\":\"Untargeted Melee Attack\",");
+								output.Append("\"targetId\":\"" + binaryReader.ReadInt32() + "\",");
+								output.Append("\"attackHeight\":\"" + binaryReader.ReadUInt32() + "\",");
+								output.Append("\"powerLevel\":\"" + binaryReader.ReadSingle() + "\"}\"");
+							}
+
+							writer.WriteLine(output);
+
+							writer.Close();
+						}
+					}
+					else if (actionId == 8) // Evt_Combat__TargetedMeleeAttack_ID
+					{
+						if (!CheckForAetheriaAndCloak())
+							return;
+
+						var logFileName = CreateFileIfItDoesntExist();
+
+						AddCurrentVariablesToLog(logFileName);
+
+						using (StreamWriter writer = new StreamWriter(logFileName, true))
+						{
+							StringBuilder output = new StringBuilder();
+
+							// "Timestamp,Landcell,JSON"
+							output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
+							output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
+
+							using (MemoryStream memoryStream = new MemoryStream(e.Message.RawData))
+							using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+							{
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+
+								var targetId = binaryReader.ReadInt32();
+
+								output.Append("\"{\"ClientDispatch\":\"Targeted Melee Attack\",");
+								output.Append("\"targetId\":\"" + targetId + "\",");
+
+								var wo = Core.WorldFilter[targetId];
+								if (targetId != null)
+									output.Append("\"targetName\":\"" + wo .Name + "\",");
+
+								output.Append("\"attackHeight\":\"" + binaryReader.ReadUInt32() + "\",");
+								output.Append("\"powerLevel\":\"" + binaryReader.ReadSingle() + "\"}\"");
+							}
+
+							writer.WriteLine(output);
+
+							writer.Close();
+						}
+					}
+					else if (actionId == 10) // Evt_Combat__TargetedMissileAttack_ID
+					{
+						if (!CheckForAetheriaAndCloak())
+							return;
+
+						var logFileName = CreateFileIfItDoesntExist();
+
+						AddCurrentVariablesToLog(logFileName);
+
+						using (StreamWriter writer = new StreamWriter(logFileName, true))
+						{
+							StringBuilder output = new StringBuilder();
+
+							// "Timestamp,Landcell,JSON"
+							output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
+							output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
+
+							using (MemoryStream memoryStream = new MemoryStream(e.Message.RawData))
+							using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+							{
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+
+								output.Append("\"{\"ClientDispatch\":\"Untargeted Missile Attack\",");
+								output.Append("\"targetId\":\"" + binaryReader.ReadInt32() + "\",");
+								output.Append("\"attackHeight\":\"" + binaryReader.ReadUInt32() + "\",");
+								output.Append("\"accuracy\":\"" + binaryReader.ReadSingle() + "\"}\"");
+							}
+
+							writer.WriteLine(output);
+
+							writer.Close();
+						}
+					}
+					else if (actionId == 11) // Evt_Combat__UntargetedMissileAttack_ID
+					{
+						if (!CheckForAetheriaAndCloak())
+							return;
+
+						var logFileName = CreateFileIfItDoesntExist();
+
+						AddCurrentVariablesToLog(logFileName);
+
+						using (StreamWriter writer = new StreamWriter(logFileName, true))
+						{
+							StringBuilder output = new StringBuilder();
+
+							// "Timestamp,Landcell,JSON"
+							output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
+							output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
+
+							using (MemoryStream memoryStream = new MemoryStream(e.Message.RawData))
+							using (BinaryReader binaryReader = new BinaryReader(memoryStream))
+							{
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+								binaryReader.ReadUInt32();
+
+								var targetId = binaryReader.ReadInt32();
+
+								output.Append("\"{\"ClientDispatch\":\"Targeted Missile Attack\",");
+								output.Append("\"targetId\":\"" + targetId + "\",");
+
+								var wo = Core.WorldFilter[targetId];
+								if (targetId != null)
+									output.Append("\"targetName\":\"" + wo.Name + "\",");
+
+								output.Append("\"attackHeight\":\"" + binaryReader.ReadUInt32() + "\",");
+								output.Append("\"accuracy\":\"" + binaryReader.ReadSingle() + "\"}\"");
+							}
+
+							writer.WriteLine(output);
+
+							writer.Close();
+						}
+					}
+				}
+			}
+			catch { }
+		}
 
 		private void EchoFilter_ServerDispatch(object sender, NetworkMessageEventArgs e)
 		{
@@ -262,7 +425,8 @@ namespace Mag_CombatLogger
 						output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
 						output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
 
-						output.Append("\"{\"EventType\":\"" + e.EventType + "\",");
+						output.Append("\"{\"Event\":\"SpellCast\",");
+						output.Append("\"EventType\":\"" + e.EventType + "\",");
 						output.Append("\"SpellId\":\"" + e.SpellId + "\",");
 						output.Append("\"TargetId\":\"" + e.TargetId + "\"}\"");
 
@@ -270,6 +434,52 @@ namespace Mag_CombatLogger
 
 						writer.Close();
 					}
+				}
+			}
+			catch { }
+		}
+
+		private int lastItemIDSelected = -1;
+
+		private void Core_ItemSelected(object sender, ItemSelectedEventArgs e)
+		{
+			try
+			{
+				var wo = Core.WorldFilter[e.ItemGuid];
+
+				if (wo == null)
+					return;
+
+				if (wo.ObjectClass != ObjectClass.Monster)
+					return;
+
+				if (e.ItemGuid == lastItemIDSelected)
+					return;
+
+				lastItemIDSelected = e.ItemGuid;
+
+				if (!CheckForAetheriaAndCloak())
+					return;
+
+				var logFileName = CreateFileIfItDoesntExist();
+
+				AddCurrentVariablesToLog(logFileName);
+
+				using (StreamWriter writer = new StreamWriter(logFileName, true))
+				{
+					StringBuilder output = new StringBuilder();
+
+					// "Timestamp,Landcell,JSON"
+					output.Append('"' + String.Format("{0:u}", DateTime.UtcNow) + ",");
+					output.Append('"' + CoreManager.Current.Actions.Landcell.ToString("X8") + '"' + ",");
+
+					output.Append("\"{\"Event\":\"ItemSelected\",");
+					output.Append("\"ItemGuid\":\"" + e.ItemGuid + "\",");
+					output.Append("\"Name\":\"" + wo.Name + "\"}\"");
+
+					writer.WriteLine(output);
+
+					writer.Close();
 				}
 			}
 			catch { }
@@ -478,16 +688,16 @@ namespace Mag_CombatLogger
 
 			// Luminance Auras
 			value = Core.CharacterFilter.GetCharProperty((int)Augmentations.AuraValor).ToString();
-			if (!variableHistory.ContainsKey("Aura.AuraValor") || variableHistory["Aura.AuraValor"] != value) { variableHistory["Aura.AuraValor"] = value; changedVariables["Aura.AuraValor"] = value; }
+			if (!variableHistory.ContainsKey("AuraValor") || variableHistory["AuraValor"] != value) { variableHistory["AuraValor"] = value; changedVariables["AuraValor"] = value; }
 
 			value = Core.CharacterFilter.GetCharProperty((int)Augmentations.AuraProtection).ToString();
-			if (!variableHistory.ContainsKey("Aura.AuraProtection") || variableHistory["Aura.AuraProtection"] != value) { variableHistory["Aura.AuraProtection"] = value; changedVariables["Aura.AuraProtection"] = value; }
+			if (!variableHistory.ContainsKey("AuraProtection") || variableHistory["AuraProtection"] != value) { variableHistory["AuraProtection"] = value; changedVariables["AuraProtection"] = value; }
 
 			value = Core.CharacterFilter.GetCharProperty((int)Augmentations.AuraGlory).ToString();
-			if (!variableHistory.ContainsKey("Aura.AuraGlory") || variableHistory["Aura.AuraGlory"] != value) { variableHistory["Aura.AuraGlory"] = value; changedVariables["Aura.AuraGlory"] = value; }
+			if (!variableHistory.ContainsKey("AuraGlory") || variableHistory["AuraGlory"] != value) { variableHistory["AuraGlory"] = value; changedVariables["AuraGlory"] = value; }
 
 			value = Core.CharacterFilter.GetCharProperty((int)Augmentations.AuraTemperance).ToString();
-			if (!variableHistory.ContainsKey("Aura.AuraTemperance") || variableHistory["Aura.AuraTemperance"] != value) { variableHistory["Aura.AuraTemperance"] = value; changedVariables["Aura.AuraTemperance"] = value; }
+			if (!variableHistory.ContainsKey("AuraTemperance") || variableHistory["AuraTemperance"] != value) { variableHistory["AuraTemperance"] = value; changedVariables["AuraTemperance"] = value; }
 
 
 			// Skills
@@ -597,17 +807,136 @@ namespace Mag_CombatLogger
 			value = Core.CharacterFilter.Burden.ToString();
 			if (!variableHistory.ContainsKey("Burden") || variableHistory["Burden"] != value) { variableHistory["Burden"] = value; changedVariables["Burden"] = value; }
 
-			// Healing? (action state)
+			// Attacking/Healing? (action state)
+			// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ADD CODE HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-			// Equipment
-			// Weapon mods & properties
-			// Shield
-			// Armor levels
+			// Weapon
+			Dictionary<string, string> weaponVariables = new Dictionary<string, string>();
+
+			foreach (var item in Core.WorldFilter.GetInventory())
+			{
+				if (item.Values(LongValueKey.EquippedSlots) < 0x00100000 || item.Values(LongValueKey.EquippedSlots) > 0x01000000)
+					continue;
+
+				if (item.Values(LongValueKey.EquippedSlots) == 0x00100000) // 00100000 -1	Melee Weapon
+				{
+					weaponVariables["meleeId"] = item.Id.ToString();
+					weaponVariables["meleeName"] = item.Name;
+				}
+
+				if (item.Values(LongValueKey.EquippedSlots) == 0x00200000) // 00200000 -1	Shield/Off Hand
+				{
+					if (item.ObjectClass == ObjectClass.MeleeWeapon)
+					{
+						weaponVariables["offHandId"] = item.Id.ToString();
+						weaponVariables["offHandName"] = item.Name;
+					}
+					else
+					{
+						weaponVariables["shieldId"] = item.Id.ToString();
+						weaponVariables["shieldName"] = item.Name;
+					}
+				}
+
+				if (item.Values(LongValueKey.EquippedSlots) == 0x00400000) // 00400000 -1	Missile Weapon
+				{
+					weaponVariables["missileId"] = item.Id.ToString();
+					weaponVariables["missileName"] = item.Name;
+				}
+
+				if (item.Values(LongValueKey.EquippedSlots) == 0x00800000) // 00800000 -1	Missile Ammo
+				{
+					weaponVariables["ammoId"] = item.Id.ToString();
+					weaponVariables["ammoName"] = item.Name;
+				}
+
+				if (item.Values(LongValueKey.EquippedSlots) == 0x01000000) // 01000000 -1	Wand/Staff/Orb
+				{
+					weaponVariables["wandId"] = item.Id.ToString();
+					weaponVariables["wandName"] = item.Name;
+				}
+
+				if (item.Values(LongValueKey.ArmorLevel) > 0)
+					weaponVariables["ArmorLevel"] = item.Values(LongValueKey.ArmorLevel).ToString();
+
+				if (item.Values(LongValueKey.Imbued) > 0)
+				{
+					if ((item.Values(LongValueKey.Imbued) & 1) == 1) weaponVariables["Imbued"] = "CS";
+					if ((item.Values(LongValueKey.Imbued) & 2) == 2) weaponVariables["Imbued"] = "CB";
+					if ((item.Values(LongValueKey.Imbued) & 4) == 4) weaponVariables["Imbued"] = "AR";
+					if ((item.Values(LongValueKey.Imbued) & 8) == 8) weaponVariables["Imbued"] = "SlashRend";
+					if ((item.Values(LongValueKey.Imbued) & 16) == 16) weaponVariables["Imbued"] = "PierceRend";
+					if ((item.Values(LongValueKey.Imbued) & 32) == 32) weaponVariables["Imbued"] = "BludgeRend";
+					if ((item.Values(LongValueKey.Imbued) & 64) == 64) weaponVariables["Imbued"] = "AcidRend";
+					if ((item.Values(LongValueKey.Imbued) & 128) == 128) weaponVariables["Imbued"] = "FrostRend";
+					if ((item.Values(LongValueKey.Imbued) & 256) == 256) weaponVariables["Imbued"] = "LightRend";
+					if ((item.Values(LongValueKey.Imbued) & 512) == 512) weaponVariables["Imbued"] = "FireRend";
+					if ((item.Values(LongValueKey.Imbued) & 1024) == 1024) weaponVariables["Imbued"] = "MeleeImbue";
+					if ((item.Values(LongValueKey.Imbued) & 4096) == 4096) weaponVariables["Imbued"] = "MagicImbue";
+					if ((item.Values(LongValueKey.Imbued) & 8192) == 8192) weaponVariables["Imbued"] = "Hematited";
+					if ((item.Values(LongValueKey.Imbued) & 536870912) == 536870912) weaponVariables["Imbued"] = "MagicAbsorb";
+				}
+
+				if (item.Values(LongValueKey.MaxDamage) != 0)
+					weaponVariables["MaxDamage"] = item.Values(LongValueKey.MaxDamage).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.Variance) != 0)
+					weaponVariables["Variance"] = item.Values(DoubleValueKey.Variance).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(LongValueKey.ElementalDmgBonus, 0) != 0)
+					weaponVariables["ElementalDmgBonus"] = item.Values(LongValueKey.ElementalDmgBonus).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.DamageBonus, 1) != 1)
+					weaponVariables["DamageBonus"] = item.Values(DoubleValueKey.DamageBonus).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.ElementalDamageVersusMonsters, 1) != 1)
+					weaponVariables["ElementalDamageVersusMonsters"] = item.Values(DoubleValueKey.ElementalDamageVersusMonsters).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.AttackBonus, 1) != 1)
+					weaponVariables["AttackBonus"] = item.Values(DoubleValueKey.AttackBonus).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.MeleeDefenseBonus, 1) != 1)
+					weaponVariables["MeleeDefenseBonus"] = item.Values(DoubleValueKey.MeleeDefenseBonus).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.MagicDBonus, 1) != 1)
+					weaponVariables["MagicDBonus"] = item.Values(DoubleValueKey.MagicDBonus).ToString(CultureInfo.InvariantCulture);
+
+				if (item.Values(DoubleValueKey.MissileDBonus, 1) != 1)
+					weaponVariables["MissileDBonus"] = item.Values(DoubleValueKey.MissileDBonus).ToString(CultureInfo.InvariantCulture);
+			}
+
+			StringBuilder weaponOutput = new StringBuilder();
+			weaponOutput.Append("{");
+			foreach (var variable in weaponVariables)
+				weaponOutput.Append("\"" + variable.Key + "\":\"" + variable.Value + "\",");
+			weaponOutput.Remove(weaponOutput.Length - 1, 1); // Remove the last comma
+			weaponOutput.Append("}");
+			if (!variableHistory.ContainsKey("Weapon") || variableHistory["Weapon"] != weaponOutput.ToString()) { variableHistory["Weapon"] = weaponOutput.ToString(); changedVariables["Weapon"] = weaponOutput.ToString(); }
+
+
+			// Armor
+			Dictionary<string, string> armorVariables = new Dictionary<string, string>();
+
+			// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ADD CODE HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+			StringBuilder armorOutput = new StringBuilder();
+			armorOutput.Append("{");
+			foreach (var variable in armorVariables)
+				armorOutput.Append("\"" + variable.Key + "\":\"" + variable.Value + "\",");
+			armorOutput.Remove(armorOutput.Length - 1, 1); // Remove the last comma
+			armorOutput.Append("}");
+			if (!variableHistory.ContainsKey("Armor") || variableHistory["Armor"] != armorOutput.ToString()) { variableHistory["Armor"] = armorOutput.ToString(); changedVariables["Armor"] = armorOutput.ToString(); }
 
 
 			// Combat Properties
-			// attack strength.. we need to get this from the actual attack... vtank holds down the button for this period of time
+			var monstersWithin3 = 0;
+			foreach (var monster in Core.WorldFilter.GetByObjectClass(ObjectClass.Monster))
+			{
+				if (Util.GetDistanceFromPlayer(monster) <= 3)
+					monstersWithin3++;
+			}
+			if (!variableHistory.ContainsKey("MonstersWithin3") || variableHistory["MonstersWithin3"] != monstersWithin3.ToString()) { variableHistory["MonstersWithin3"] = monstersWithin3.ToString(); changedVariables["MonstersWithin3"] = monstersWithin3.ToString(); }
 
 
 			// Target
@@ -627,7 +956,12 @@ namespace Mag_CombatLogger
 					output.Append("\"{");
 
 					foreach (var variable in changedVariables)
-						output.Append("\"" + variable.Key + "\":\"" + variable.Value + "\",");
+					{
+						if (variable.Value.StartsWith("{"))
+							output.Append("\"" + variable.Key + "\":" + variable.Value + ",");
+						else
+							output.Append("\"" + variable.Key + "\":\"" + variable.Value + "\",");
+					}
 
 					// Remove the last comma
 					output.Remove(output.Length - 1, 1);
